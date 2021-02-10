0111a471b5831d6942b7188015f7bd6c6358e05b
camilo <rodrigo.camilo@gmail.com>
2021-02-10 04:33:02 +0100

    C1D2 controle da janela e botao ESC

--- srcs/main.c
+++ srcs/main.c
@@ -2,44 +2,51 @@
 #include "mlx.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void line(void *mlx, void *win, int x, int y, int size)
+int	create_trgb(int t, int r, int g, int b)
+{
+	return(t << 24 | r << 16 | g << 8 | b);
+}
+
+
+void line(void *mlx, void *win, int x, int y, int size, int color)
 {
 	int i;
 
 	i = 0;
 	while (i < size){
-		mlx_pixel_put(mlx, win, x + i, y, 0255255255);
+		mlx_pixel_put(mlx, win, x + i, y, color);
 		i++;
 	}
 
 }
 
-void 	square(void *mlx, void *window, int x, int y, int size)
+void 	square(void *mlx, void *window, int x, int y, int size, int color)
 {
 	int i;
 
 	i= 0;
 	while (i <= size)
 	{
-		mlx_pixel_put(mlx, window, x + i, y, 0777777777);
-		mlx_pixel_put(mlx, window, x, y + i, 0777777777);
-		mlx_pixel_put(mlx, window, x + size, y + i, 0777777777);
-		mlx_pixel_put(mlx, window, x + i, y + size, 0777777777);
+		mlx_pixel_put(mlx, window, x + i, y, color);
+		mlx_pixel_put(mlx, window, x, y + i, color);
+		mlx_pixel_put(mlx, window, x + size, y + i, color);
+		mlx_pixel_put(mlx, window, x + i, y + size, color);
 		i++;
 	}
 
 }
 
 
-void 	filled_square(void *mlx, void *window, int x, int y, int size)
+void 	filled_square(void *mlx, void *window, int x, int y, int size, int color)
 {
 	int i;
 
 	i= 0;
 	while (i <= size)
 	{
-		line(mlx, window, x, y + i , size);
+		line(mlx, window, x, y + i , size, color);
 		i++;
 	}
 
@@ -67,30 +74,32 @@ void	circle(void *mlx, void *window, int x, int y, int raio)
 	}
 }
 
-void 	draw_map(void *mlx, void *window, int map[5][5], int tile_size, int nr_tiles){
+void 	draw_map(void *mlx, void *window, int ytiles, int xtiles, int map[ytiles][xtiles], int tile_size){
 	int i;
 	int j;
 	int x;
 	int y;
+	int black;
+	int white;
+
+	black = create_trgb(0, 0, 0, 0);
+	white = create_trgb(0, 255, 255, 255);
 
 	i = 0;
-	while (i < nr_tiles)
+	while (i < ytiles)
 	{
 		j = 0;
-		while (j < nr_tiles)
+		while (j < xtiles)
 		{
 			x = j * tile_size;
 			y = i * tile_size;
 			if (map[i][j] == 1)
-				{
-				printf("a");
-				filled_square(mlx, window, x, y, tile_size);
-				}
+			{
+				filled_square(mlx, window, x, y, tile_size, white);
+			}
 			else
 			{
-				printf("b");
-				square(mlx, window, x, y, tile_size);
-
+				square(mlx, window, x, y, tile_size, black);
 			}
 			j++;
 		}
@@ -99,34 +108,105 @@ void 	draw_map(void *mlx, void *window, int map[5][5], int tile_size, int nr_til
 
 }
 
+typedef struct Player{
+	float x;
+	float y;
+	float size;
+	int turnDirection;
+	int walkDirection;
+	float RotationAngle;
+	float walkSpeed;
+	float turnSpeed;
+} player;
 
+void init_player(player *player)
+{
+	player->x = 50;
+	player->y = 50;
+	player->size = 5;
+	player->turnDirection = 0;
+	player->walkDirection = 0;
+	player->RotationAngle = 3.14/2;
+	player->walkSpeed = 10;
+	player->turnSpeed = 45 * (3.14/2);
+
+}
+
+
+
+typedef struct  s_vars {
+        void    *mlx;
+        void    *win;
+}               t_vars;
+
+
+
+int close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	vars->win = NULL;
+	exit(1);
+	return (0);
+}
+
+int keypress(int keycode, t_vars *vars)
+{
+    if (keycode == 53 || keycode == 65307)
+	{
+		close(vars);
+	}
+	return (0);
+}
 
 
 int main(){
-	void *mlx_ptr;
-	void *win_ptr;
-	int map[5][5] = {
-		{1, 1, 1, 1, 1},
-		{1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1},
-		{1, 1, 1, 1, 1}
+	player player;
+	t_vars    vars;
+	int red;
+	int map[14][33] = {
+{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,2,0,0,0,0,0,1},
+{1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+{1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1},
+{1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,0,1,1,1,1,1},
+{1,1,0,0,0,0,0,0,1,1,0,1,0,1,0,1,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1},
+{1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1},
+{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,0,1,0,0,0,1,1,1,1,1},
+{1,1,0,0,0,0,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,1,1,1,0,0,0,1,1,1,1,1},
+{1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,0,1,0,0,0,1,1,1,1,1},
+{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 	};
 
 
+	red = create_trgb(0, 255, 0, 0);
+	init_player(&player);
 
 
 
-
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	//mlx_string_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF, "Rodrigo");
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 800, 500, "mlx 42");
+	//mlx_string_put(mlx_ptr, win_ptr, 250, 250, red, "Rodrigo");
 	//line(mlx_ptr, win_ptr, 260, 260, 30);
 	//square(mlx_ptr, win_ptr, 260, 270, 30);
 	//circle(mlx_ptr, win_ptr, 250, 250, 30);
 	//filled_square(mlx_ptr, win_ptr, 260, 270, 30);
-	draw_map(mlx_ptr, win_ptr, map, 100, 5);
+	draw_map(vars.mlx, vars.win, 14, 33, map, 20);
 
-	mlx_loop(mlx_ptr);
+	printf("%f, %f", player.x, player.y);
+	filled_square(vars.mlx, vars.win, player.x, player.y, player.size, red);
+
+
+	mlx_hook(vars.win, 2, 1L<<0, keypress, &vars);
+	mlx_hook(vars.win, 33, 1L<<17, close, &vars);
+
+	mlx_loop(vars.mlx);
+
+	// analisar memory leak no final
+	//mlx_destroy_display(vars.mlx); 
+	//free(vars.mlx);
+	return (0);
 
 }
